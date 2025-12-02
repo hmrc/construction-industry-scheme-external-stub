a63af173a2732f700b75c5f20197b415d02ce1c2
Richy Jassal <20478717+jassalrichy@users.noreply.github.com>
2025-12-02 16:15:05 +0000

    [DTR-268] - success no receipt routing (#7)
    * [DTR-268] - success no receipt routing
    
    * [DTR-268] - dynamically update responses with the request correlationId

--- app/uk/gov/hmrc/constructionindustryschemeexternalstub/controllers/chris/ChrisController.scala
+++ app/uk/gov/hmrc/constructionindustryschemeexternalstub/controllers/chris/ChrisController.scala
@@ -22,22 +22,73 @@ import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import uk.gov.hmrc.constructionindustryschemeexternalstub.config.AppConfig
 import uk.gov.hmrc.constructionindustryschemeexternalstub.services.ChrisService
 import uk.gov.hmrc.constructionindustryschemeexternalstub.models.{FATAL_ERROR, SUCCESS}
+import uk.gov.hmrc.constructionindustryschemeexternalstub.utils.ResourceHelper
 
 import javax.inject.{Inject, Singleton}
-import scala.xml.NodeSeq
+import scala.xml.{Node, NodeSeq}
 
 @Singleton()
-class ChrisController @Inject() (service: ChrisService, config: AppConfig, cc: ControllerComponents)
-    extends BackendController(cc) {
+class ChrisController @Inject() (
+  service: ChrisService,
+  config: AppConfig,
+  resourceHelper: ResourceHelper,
+  cc: ControllerComponents
+) extends BackendController(cc) {
   private val logger = Logger(classOf[ChrisController])
 
+  private val monthlyNilReturnResponsePath                      = "/resources/monthlyNilReturns"
+  private val submitCISMessage_acknowledgement_ResponsePath     =
+    s"$monthlyNilReturnResponsePath/submitCISMessage-acknowledgement-response.xml"
+  private val submitCISMessage_success_ResponsePath             =
+    s"$monthlyNilReturnResponsePath/submitCISMessage-success-response.xml"
+  private val submitCISMessage_fatalError_ResponsePath          =
+    s"$monthlyNilReturnResponsePath/submitCISMessage-fatalError-response.xml"
+  private val submitCISMessage_businessError_ResponsePath       =
+    s"$monthlyNilReturnResponsePath/submitCISMessage-businessError-response.xml"
+  private val submitCISMessage_irMarkMismatchError_ResponsePath =
+    s"$monthlyNilReturnResponsePath/submitCISMessage-irMarkMismatchError-response.xml"
+
   def submitCISMessage(): Action[AnyContent] = Action { (request: Request[AnyContent]) =>
-    val rootTextOpt = service.responseCISMessage(request.body.asXml.get)
-    if (rootTextOpt.isDefined) {
-      Ok(rootTextOpt.get)
-    } else {
-      NotFound
+    val message                           = request.body.asXml.get
+    val correlationId                     = (message \ "Header" \ "MessageDetails" \ "CorrelationID").text
+    val keys                              = message \ "GovTalkDetails" \ "Keys" \ "Key"
+    def typeIs(value: String)(node: Node) = node \@ "Type" == value
+    val taxOfficeNumber                   = (keys filter typeIs("TaxOfficeNumber")).text match {
+      case text if text.nonEmpty => text
+      case _                     => "123"
     }
+    val taxOfficeReference                = (keys filter typeIs("TaxOfficeReference")).text
+
+    (taxOfficeNumber, taxOfficeReference) match {
+      case ("754", "EZ00100") =>
+        Ok(
+          replaceCorrelationId(
+            resourceHelper.resourceAsString(submitCISMessage_acknowledgement_ResponsePath),
+            correlationId
+          )
+        )
+      case ("754", "EZ00125") =>
+        Ok(
+          replaceCorrelationId(resourceHelper.resourceAsString(submitCISMessage_fatalError_ResponsePath), correlationId)
+        )
+      case ("754", "EZ00150") =>
+        Ok(
+          replaceCorrelationId(
+            resourceHelper.resourceAsString(submitCISMessage_businessError_ResponsePath),
+            correlationId
+          )
+        )
+      case ("754", "EZ00200") =>
+        Ok(
+          replaceCorrelationId(
+            resourceHelper.resourceAsString(submitCISMessage_irMarkMismatchError_ResponsePath),
+            correlationId
+          )
+        )
+      case _                  =>
+        Ok(replaceCorrelationId(resourceHelper.resourceAsString(submitCISMessage_success_ResponsePath), correlationId))
+    }
+
   }
 
   def submitCISVerifyMessage(): Action[AnyContent] = Action { (request: Request[AnyContent]) =>
@@ -104,5 +155,8 @@ class ChrisController @Inject() (service: ChrisService, config: AppConfig, cc: C
     )
     Ok(rootTextOpt.get)
   }
+
+  private def replaceCorrelationId(response: String, correlationId: String): String =
+    response.replace("[correlationId]", correlationId)
 
 }

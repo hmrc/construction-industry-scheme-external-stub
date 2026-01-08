664b5b504abf131b5a2fd3f16fa6e6ce61a021e8
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2026-01-08 12:56:56 +0000

    DTR-1778: create subcontractor stubs added

---
+++ app/uk/gov/hmrc/constructionindustryschemeexternalstub/controllers/formpProxy/SubcontractorController.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.constructionindustryschemeexternalstub.controllers.formpProxy
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, ControllerComponents}
+import uk.gov.hmrc.constructionindustryschemeexternalstub.actions.AuthAction
+import uk.gov.hmrc.constructionindustryschemeexternalstub.models.requests.SubcontractorCreateRequest
+import uk.gov.hmrc.constructionindustryschemeexternalstub.utils.{EnrolmentsHelper, ResourceHelper}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+
+class SubcontractorController @Inject() (
+  authorise: AuthAction,
+  resourceHelper: ResourceHelper,
+  enrolmentHelper: EnrolmentsHelper,
+  cc: ControllerComponents
+)() extends BackendController(cc)
+    with Logging {
+
+  private val subcontractorResponsePath            = "/resources/subcontractor"
+  private val createSubcontractor_201_ResponsePath = s"$subcontractorResponsePath/createSubcontractor-201-response.json"
+
+  def createSubContractor: Action[SubcontractorCreateRequest] =
+    authorise(parse.json[SubcontractorCreateRequest]) { implicit request =>
+      val enrolments = enrolmentHelper.contractorEnrolmentsOpt(request)
+      enrolments match {
+        case Some(enrolmentReference) =>
+          (enrolmentReference.taxOfficeNumber, enrolmentReference.taxOfficeReference) match {
+            case ("500", _) => InternalServerError(Json.obj("message" -> "Unexpected error"))
+            case ("502", _) => BadGateway(Json.obj("message" -> "formp failed"))
+            case _          => Created(resourceHelper.resourceAsString(createSubcontractor_201_ResponsePath))
+          }
+        case None                     => InternalServerError
+      }
+    }
+}
---
+++ test/uk/gov/hmrc/constructionindustryschemeexternalstub/controllers/formpProxy/SubcontractorControllerSpec.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.constructionindustryschemeexternalstub.controllers.formpProxy
+
+import org.mockito.ArgumentMatchers.*
+import org.mockito.Mockito.*
+import org.scalatest.freespec.AnyFreeSpec
+import play.api.http.Status.CREATED
+import play.api.libs.json.{JsObject, JsValue, Json}
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import uk.gov.hmrc.constructionindustryschemeexternalstub.actions.FakeAuthAction
+import uk.gov.hmrc.constructionindustryschemeexternalstub.base.SpecBase
+import uk.gov.hmrc.constructionindustryschemeexternalstub.models.EmployerReference
+import uk.gov.hmrc.constructionindustryschemeexternalstub.models.requests.{CreateNilMonthlyReturnRequest, SubcontractorCreateRequest}
+import uk.gov.hmrc.constructionindustryschemeexternalstub.utils.{EnrolmentsHelper, ResourceHelper}
+
+import scala.concurrent.Future
+
+class SubcontractorControllerSpec extends SpecBase {
+  ".createSubmission" - {
+
+    val createSubmissionUrl = "/cis/subcontractor/create"
+
+    "returns 201 Created with subbieResourceRef on valid payload" in new Setup {
+
+      val request = SubcontractorCreateRequest(
+        schemeId = 1,
+        subcontractorType = "trader",
+        currentVersion = 0
+      )
+
+      val response: JsObject = Json.obj("subbieResourceRef" -> 10)
+
+      when(mockEnrolmentsHelper.contractorEnrolmentsOpt(any()))
+        .thenReturn(Some(EmployerReference("200", "")))
+
+      when(mockResourceHelper.resourceAsString(any()))
+        .thenReturn(response.toString)
+
+      val req: FakeRequest[SubcontractorCreateRequest] =
+        FakeRequest(POST, createSubmissionUrl).withBody(request)
+
+      val res: Future[Result] = controller.createSubContractor(req)
+
+      status(res) mustBe CREATED
+      contentAsJson(res) mustBe response
+    }
+
+    "propagates UpstreamErrorResponse for taxOfficeNumber = 502" in new Setup {
+
+      val request = SubcontractorCreateRequest(
+        schemeId = 1,
+        subcontractorType = "trader",
+        currentVersion = 0
+      )
+
+      when(mockEnrolmentsHelper.contractorEnrolmentsOpt(any()))
+        .thenReturn(Some(EmployerReference("502", "")))
+
+      val req: FakeRequest[SubcontractorCreateRequest] =
+        FakeRequest(POST, createSubmissionUrl).withBody(request)
+
+      val res: Future[Result] = controller.createSubContractor(req)
+
+      status(res) mustBe BAD_GATEWAY
+      (contentAsJson(res) \ "message").as[String] must include("formp failed")
+
+    }
+
+    "returns 500 with generic message for taxOfficeNumber = 500" in new Setup {
+
+      val request = SubcontractorCreateRequest(
+        schemeId = 1,
+        subcontractorType = "trader",
+        currentVersion = 0
+      )
+
+      when(mockEnrolmentsHelper.contractorEnrolmentsOpt(any()))
+        .thenReturn(Some(EmployerReference("500", "")))
+
+      val req: FakeRequest[SubcontractorCreateRequest] =
+        FakeRequest(POST, createSubmissionUrl).withBody(request)
+
+      val res: Future[Result] = controller.createSubContractor(req)
+
+      status(res) mustBe INTERNAL_SERVER_ERROR
+      (contentAsJson(res) \ "message").as[String] mustBe "Unexpected error"
+    }
+  }
+
+  private trait Setup {
+    val mockResourceHelper: ResourceHelper     = mock[ResourceHelper]
+    val mockEnrolmentsHelper: EnrolmentsHelper = mock[EnrolmentsHelper]
+
+    val auth: FakeAuthAction = new FakeAuthAction(cc.parsers)
+    lazy val controller      = new SubcontractorController(auth, mockResourceHelper, mockEnrolmentsHelper, cc)
+
+    def makeJsonRequest(body: JsValue, url: String): FakeRequest[JsValue] =
+      FakeRequest(POST, "/cis/subcontractor")
+        .withHeaders(CONTENT_TYPE -> JSON, ACCEPT -> JSON)
+        .withBody(body)
+  }
+}

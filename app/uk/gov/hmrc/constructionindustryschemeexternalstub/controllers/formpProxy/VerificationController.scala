db3a072ba817c6c3b97066f3881c97dce61a5263
codeneto-hmrc <253322371+codeneto-hmrc@users.noreply.github.com>
2026-04-13 08:10:29 +0100

    DTR-4160: Retrieve latest verification batch

---
+++ app/uk/gov/hmrc/constructionindustryschemeexternalstub/controllers/formpProxy/VerificationController.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.constructionindustryschemeexternalstub.controllers.formpProxy
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.constructionindustryschemeexternalstub.actions.AuthAction
+import uk.gov.hmrc.constructionindustryschemeexternalstub.utils.{EnrolmentsHelper, ResourceHelper}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+
+class VerificationController @Inject() (
+                                         authorise: AuthAction,
+                                         resourceHelper: ResourceHelper,
+                                         enrolmentHelper: EnrolmentsHelper,
+                                         cc: ControllerComponents
+                                       )() extends BackendController(cc)
+  with Logging {
+
+  private val verificationResponsePath = "/resources/verification"
+  private val getNewestVerificationBatch_200_ResponsePath =
+    s"$verificationResponsePath/getNewestVerificationBatch-200-response.json"
+
+  def getNewestVerificationBatch(instanceId: String): Action[AnyContent] =
+    authorise { implicit request =>
+      enrolmentHelper.contractorEnrolmentsOpt(request) match {
+        case Some(enrolmentReference) =>
+          (enrolmentReference.taxOfficeNumber, enrolmentReference.taxOfficeReference) match {
+            case ("500", _) => InternalServerError(Json.obj("message" -> "Unexpected error"))
+            case ("502", _) => BadGateway(Json.obj("message" -> "formp failed"))
+            case _ =>
+              Ok(Json.parse(resourceHelper.resourceAsString(getNewestVerificationBatch_200_ResponsePath)))
+          }
+        case None =>
+          InternalServerError
+      }
+    }
+}

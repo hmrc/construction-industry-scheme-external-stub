d13f3be989f345c8ae8fb7d5f727a5db1fd9799a
codeneto-hmrc <253322371+codeneto-hmrc@users.noreply.github.com>
2026-04-22 11:26:14 +0100

    DTR-4160:created ContractorSchemeNewVerificationSpec for testing

--- test/uk/gov/hmrc/constructionindustryschemeexternalstub/models/ContractorSchemeNewVerificationSpec.scala
+++ test/uk/gov/hmrc/constructionindustryschemeexternalstub/models/ContractorSchemeNewVerificationSpec.scala
@@ -16,4 +16,78 @@
 
 package uk.gov.hmrc.constructionindustryschemeexternalstub.models
 
-case class ContractorSchemeNewVerificationSpec()
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.{JsSuccess, Json}
+
+final class ContractorSchemeNewVerificationSpec extends AnyWordSpec with Matchers {
+
+  "ContractorSchemeNewVerification JSON format" should {
+
+    "read JSON into model (including missing optional fields)" in {
+      val json = Json.parse(
+        """
+          |{
+          |  "accountsOfficeReference": "123PA12345678"
+          |}
+          |""".stripMargin
+      )
+
+      val result = json.validate[ContractorSchemeNewVerification]
+      result mustBe a[JsSuccess[?]]
+
+      val out = result.get
+      out.accountsOfficeReference mustBe Some("123PA12345678")
+      out.utr mustBe None
+      out.name mustBe None
+      out.emailAddress mustBe None
+    }
+
+    "read JSON into model when all fields are present" in {
+      val json = Json.parse(
+        """
+          |{
+          |  "accountsOfficeReference": "123PA12345678",
+          |  "utr": "1234567890",
+          |  "name": "ABC Construction Ltd",
+          |  "emailAddress": "test@example.com"
+          |}
+          |""".stripMargin
+      )
+
+      val out = json.as[ContractorSchemeNewVerification]
+      out.accountsOfficeReference mustBe Some("123PA12345678")
+      out.utr mustBe Some("1234567890")
+      out.name mustBe Some("ABC Construction Ltd")
+      out.emailAddress mustBe Some("test@example.com")
+    }
+
+    "write model to JSON" in {
+      val model = ContractorSchemeNewVerification(
+        accountsOfficeReference = Some("123PA12345678"),
+        utr = Some("1234567890"),
+        name = Some("ABC Construction Ltd"),
+        emailAddress = Some("test@example.com")
+      )
+
+      val json = Json.toJson(model)
+
+      (json \ "accountsOfficeReference").as[String] mustBe "123PA12345678"
+      (json \ "utr").as[String] mustBe "1234567890"
+      (json \ "name").as[String] mustBe "ABC Construction Ltd"
+      (json \ "emailAddress").as[String] mustBe "test@example.com"
+    }
+
+    "round-trip (model -> json -> model) without losing data" in {
+      val model = ContractorSchemeNewVerification(
+        accountsOfficeReference = None,
+        utr = None,
+        name = Some("Only Name"),
+        emailAddress = None
+      )
+
+      val json = Json.toJson(model)
+      json.validate[ContractorSchemeNewVerification] mustBe JsSuccess(model)
+    }
+  }
+}

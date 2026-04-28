fbbb7e5c547c38841db66b10d5fce9482a70d0ad
Edward Pau <232416446+edpau-hmrc@users.noreply.github.com>
2026-04-28 14:04:03 +0100

    DTR-4162: create stub for Business Function F7: Retrieve current verification batch (#69)
    * DTR-4162: create stub for Business Function F7: Retrieve current verification batch
    
    * DTR-4162: apply format
    
    * DTR-4162: fix app.routes after merge main
    
    * DTR-4162: add partnershipTradingName field
    
    * DTR-4162: change _CurrVerification to _CurrentVerification

---
+++ test/uk/gov/hmrc/constructionindustryschemeexternalstub/models/VerificationCurrentVerificationSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.constructionindustryschemeexternalstub.models
+
+import uk.gov.hmrc.constructionindustryschemeexternalstub.base.SpecBase
+import play.api.libs.json.Json
+
+class VerificationCurrentVerificationSpec extends SpecBase {
+  "VerificationCurrentVerification" - {
+    "serialize to JSON correctly" in {
+      val verification = VerificationCurrentVerification(
+        verificationId = 1L,
+        verificationBatchId = Some(10L),
+        subcontractorId = Some(2L),
+        verificationResourceRef = Some(20L)
+      )
+      val json         = Json.toJson(verification)
+
+      (json \ "verificationId").as[Long] mustBe 1L
+      (json \ "verificationBatchId").as[Long] mustBe 10L
+      (json \ "subcontractorId").as[Long] mustBe 2L
+      (json \ "verificationResourceRef").as[Long] mustBe 20L
+    }
+    "deserialize from JSON correctly" in {
+      val json   = Json.parse(
+        """
+          |{
+          |  "verificationId": 1,
+          |  "verificationBatchId": 10,
+          |  "subcontractorId": 2,
+          |  "verificationResourceRef": 20
+          |}
+          |""".stripMargin
+      )
+      val result = json.as[VerificationCurrentVerification]
+      result.verificationId mustBe 1L
+      result.verificationBatchId mustBe Some(10L)
+      result.subcontractorId mustBe Some(2L)
+      result.verificationResourceRef mustBe Some(20L)
+    }
+    "round-trip serialize and deserialize correctly" in {
+      val verification = VerificationCurrentVerification(
+        verificationId = 1L,
+        verificationBatchId = Some(10L),
+        subcontractorId = Some(2L),
+        verificationResourceRef = Some(20L)
+      )
+      val json         = Json.toJson(verification)
+      val result       = json.as[VerificationCurrentVerification]
+      result mustBe verification
+    }
+  }
+}

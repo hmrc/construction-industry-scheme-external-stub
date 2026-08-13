eb310abec76a2f606fd40d9a923dc0c9538ecf37
pvarsani <243989389+pvarsani@users.noreply.github.com>
2026-08-13 10:40:19 +0100

    fix unit test

--- test/uk/gov/hmrc/constructionindustryschemeexternalstub/controllers/formpProxy/SubcontractorControllerSpec.scala
+++ test/uk/gov/hmrc/constructionindustryschemeexternalstub/controllers/formpProxy/SubcontractorControllerSpec.scala
@@ -359,7 +359,7 @@ class SubcontractorControllerSpec extends SpecBase {
       )
     }
 
-    "returns subcontractor details with canBeDeleted = false for resource ref 27" in new Setup {
+    "returns subcontractor details with canBeDeleted = false for resource ref 7" in new Setup {
 
       val req =
         FakeRequest(
@@ -368,12 +368,12 @@ class SubcontractorControllerSpec extends SpecBase {
         )
 
       val res =
-        controller.getSubcontractorForDelete(deleteStatusCisId, 27L)(req)
+        controller.getSubcontractorForDelete(deleteStatusCisId, 7L)(req)
 
       status(res) mustBe OK
 
       contentAsJson(res) mustBe Json.obj(
-        "subcontractorName"         -> "Gamma Builders",
+        "subcontractorName"         -> "Delta Trust",
         "subcontractorCanBeDeleted" -> false
       )
     }
